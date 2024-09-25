eb82f47027e9e5e96334c9803f21f4694a7d8544
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-09-25 20:35:10 +0000

    plex friendly file names update

--- bot.py
+++ bot.py
@@ -33,9 +33,8 @@ recent_messages = deque(maxlen=100)
 
 
 def sanitize_name(name):
-    # Replace spaces with hyphens and convert to lowercase
-    # Remove any characters that aren't alphanumeric, hyphens, or underscores
-    return re.sub(r"[^\w\-]", "", name.replace(" ", "-")).lower()
+    name = re.sub(r"\.", " ", name)
+    return re.sub(r"[^\w\s-]", "", name).strip()
 
 
 def get_dynamic_path(file_name):
@@ -47,11 +46,9 @@ def get_dynamic_path(file_name):
         show_name = sanitize_name(tv_match.group(1).strip())
         season = int(tv_match.group(2))
         episode = int(tv_match.group(3))
-        plex_file_name = (
-            f"{show_name.title()} - s{season:02d}e{episode:02d}{extension.lower()}"
-        )
+        plex_file_name = f"{show_name} - s{season:02d}e{episode:02d}{extension.lower()}"
         return os.path.join(
-            "tv-shows", show_name.title(), f"Season {season:02d}", plex_file_name
+            "tv-shows", show_name, f"Season {season:02d}", plex_file_name
         )
     else:
         # If not a TV show, assume it's a movie
@@ -59,10 +56,10 @@ def get_dynamic_path(file_name):
         if movie_match:
             movie_name = sanitize_name(movie_match.group(1).strip())
             year = movie_match.group(2)
-            plex_file_name = f"{movie_name.title()} ({year}){extension.lower()}"
+            plex_file_name = f"{movie_name} ({year}){extension.lower()}"
         else:
             movie_name = sanitize_name(name)
-            plex_file_name = f"{movie_name.title()}{extension.lower()}"
+            plex_file_name = f"{movie_name}{extension.lower()}"
         return os.path.join("movies", plex_file_name)
 
 
